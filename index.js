2185b81dd4acda8ca4f9de7cce2c3b8d2642eea1
Euginium <42642146+euginium@users.noreply.github.com>
2022-04-04 19:55:12 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,20 +1,101 @@
+const basePath = process.cwd();
 const fs = require("fs");
+const buildDir = `${basePath}`;
 
-let initialInvestment = 98;
-let dailyROIPercentage = 1.06;
-let numberOfDays = 90;
-let currency = "USD";
-let bnbPrice = 440;
+
+// configure variables here
+let initialInvestment = 98; // your initial investment
+let dailyROIPercentage = 1.05;// daily compounding percentage 1.05 = 5%
+let numberOfDays = 90;// number of days of compounding
+let currency = "USD"; // currency ticker
+let bnbPrice = 440;// current BNB price
 let profit;
+const compoundTable = [
+    {
+        Day: "",
+        Generated: "",
+        BnbDailyRewards: "",
+        DailyROIReward: ""
+    }
+] ;
 
-for(i=0; i <= numberOfDays; i++){
-    profit = initialInvestment*dailyROIPercentage;
-
-    let dailyROI = profit - initialInvestment;
-    let bnbDailyROI = dailyROI/bnbPrice;
-    initialInvestment =profit;
-    let bnb = profit/440;
+// function to create json file
+const writeList = (_data) => {
+    fs.writeFileSync(`${buildDir}/_list.json`, _data);
+  };
 
 
-    console.log(`Day ${i} you have generated : ${currency} ${profit.toFixed(2)} (inclusive of initial investment)  - BNB daily rewards = ${bnbDailyROI} BNB(USD ${dailyROI})`);
+// This function generates with json file.
+const generateWithJSON = () => {
+    for(i=0; i <= numberOfDays; i++){
+        profit = initialInvestment*dailyROIPercentage;
+    
+        let dailyROI = profit - initialInvestment;
+        let bnbDailyROI = dailyROI/bnbPrice;
+        initialInvestment =profit;
+        let bnb = profit/440;
+    
+        compoundTable.push(
+            {
+                Day: i,
+                Generated: `${currency} ${profit}`,
+                BnbDailyRewards: `BNB ${bnbDailyROI}`,
+                DailyROIReward: `${currency} ${dailyROI}`
+            }
+        )
+    
+        console.log(`Day ${i} you have generated : ${currency} ${profit.toFixed(2)} (inclusive of initial investment)  - BNB daily rewards = ${bnbDailyROI} BNB(USD ${dailyROI})`);
+    
+        writeList(JSON.stringify(compoundTable, null, 2));
+    }    
 }
+
+
+// this function generates without JSON FILE. Results are only shown in terminal
+const generateWithoutJSON = () => {
+
+    for(i=0; i <= numberOfDays; i++){
+        
+        profit = initialInvestment*dailyROIPercentage;
+        
+        let dailyROI = profit - initialInvestment;
+        
+        let bnbDailyROI = dailyROI/bnbPrice;
+       
+        initialInvestment =profit;
+        
+        let bnb = profit/440;
+    
+        console.log(`Day ${i} you have generated : ${currency} ${profit.toFixed(2)} (inclusive of initial investment)  - BNB daily rewards = ${bnbDailyROI} BNB(USD ${dailyROI})`);
+    
+    }    
+}
+
+
+// use the function below either : generateWithJSON(); or generateWithoutJSON();
+generateWithJSON();
+
+
+// read json data
+let rawdata = fs.readFileSync(`${buildDir}/_list.json`);
+let data = JSON.parse(rawdata);
+
+
+
+// function to create json file for profit days
+const profitTable = [];
+
+const writeProfitList = (_data) => {
+    fs.writeFileSync(`${buildDir}/_profitList.json`, _data);
+  };
+//calculate rewards every week
+let profitDay = 7+1
+for(i= profitDay; i <90; i+=7){
+    console.log(`Day : ${data[i].Day} , BNB rewards:${data[i].BnbDailyRewards}`);
+
+    let _result =`Day : ${data[i].Day},  BNB rewards:${data[i].BnbDailyRewards}`;
+    profitTable.push(_result);
+    writeProfitList(JSON.stringify(profitTable, null, 2));
+
+}
+
